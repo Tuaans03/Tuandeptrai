51abac4877247c64834f73ca9aa31299a3df7c74
Tuaans03 <nguyenquoctuan.work@gmail.com>
2022-08-29 23:27:05 +0700

    first commit

---
+++ #1GuessNumber/main.js
@@ -0,0 +1,62 @@
+let number = Math.trunc(Math.random()*20)+1;
+
+// document.querySelector('.number').textContent=number;
+
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.check').addEventListener('click',function () {
+
+    const guess =Number(document.querySelector('.guess').value);
+    console.log(guess,typeof guess);
+
+    if(!guess){
+        document.querySelector('.message').textContent = '❌ No Number!';
+    }
+
+    else if ( guess === number){
+        document.querySelector('.message').textContent = '🎉 Corect Number!';
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem'
+
+        if(score>highscore){
+            highscore=score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+    }
+
+
+    else if (guess >number){
+        if(score>0){
+        document.querySelector('.message').textContent = "Too high!";
+        --score;
+        document.querySelector('.score').textContent = score;
+    }
+    else{
+        document.querySelector('.message').textContent = 'You lost the game!💣';
+    }
+    }
+
+
+    else if(guess<number){
+        if(score>0){
+            document.querySelector('.message').textContent = "Too low!";
+            --score;
+            document.querySelector('.score').textContent = score;
+        }
+        else{
+            document.querySelector('.message').textContent = 'You lost the game!💣';
+        }
+    }
+    
+});
+
+document.querySelector('.again').addEventListener('click',function(){
+    score= 20;
+    number = Math.trunc(Math.random()*20)+1;
+    document.querySelector('.message').textContent = 'Start Guessing...';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15 rem';
+})
